831172d39f9b1754c81c7c86762e0ef54f61a937
Glenn Neidermeier <neiderm@github.com>
2024-05-15 19:25:11 -0400

    Update MMCM output frequency for better accuracy

--- galaxian_papilio/scripts/build_roms_galaxian.sh
+++ galaxian_papilio/scripts/build_roms_galaxian.sh
@@ -38,7 +38,10 @@ romgen_path=../romgen_source
 # REM %romgen_path%\romgen %rom_path_src%\mc_wav_2.bin GALAXIAN_WAV 18 l r e > %rom_path%\galaxian_wav.vhd
 
 # REM concatenate consecutive ROM regions
-cat $rom_path_src/1h.bin $rom_path_src/1k.bin > $rom_path/gfx1.bin
+
+#
+#cat $rom_path_src/1h.bin $rom_path_src/1k.bin > $rom_path/gfx1.bin
+
 cat $rom_path_src/galmidw.u $rom_path_src/galmidw.v $rom_path_src/galmidw.w $rom_path_src/galmidw.y $rom_path_src/7l > $rom_path/main.bin
  
 # REM generate RTL code for small PROMS
@@ -46,7 +49,7 @@ cat $rom_path_src/galmidw.u $rom_path_src/galmidw.v $rom_path_src/galmidw.w $rom
 $romgen_path/romgen $rom_path_src/6l.bpr    GALAXIAN_6L  5 l r e     > $rom_path/galaxian_6l.vhd
 
 # REM generate RAMB structures for larger ROMS
-$romgen_path/romgen $rom_path/gfx1.bin        GFX1      12 l r e > $rom_path/gfx1.vhd
+#$romgen_path/romgen $rom_path/gfx1.bin        GFX1      12 l r e > $rom_path/gfx1.vhd
 $romgen_path/romgen $rom_path/main.bin        ROM_PGM_0 14 l r e > $rom_path/rom0.vhd
 
 $romgen_path/romgen $rom_path_src/1h.bin    GALAXIAN_1H 11 l r e > $rom_path/galaxian_1h.vhd
